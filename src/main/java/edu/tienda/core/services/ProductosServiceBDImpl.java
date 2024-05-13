1ee45524972397bd802f0053383899183eeee0f5
RaulADSI <rauramirez25@gmail.com>
2024-05-13 11:11:56 -0500

    Implementamos la conexion a BD

---
+++ src/main/java/edu/tienda/core/services/ProductosServiceBDImpl.java
@@ -0,0 +1,39 @@
+package edu.tienda.core.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import edu.tienda.core.domain.Producto;
+import edu.tienda.core.persistance.entities.ProductoEntity;
+import edu.tienda.core.persistance.repositories.ProductosRepository;
+
+
+@Service("BD")
+@ConditionalOnProperty(value = "productos.estrategia", havingValue = "EN_BD")
+public class ProductosServiceBDImpl implements ProductoService {
+
+    @Autowired
+    private ProductosRepository productosRepository;
+
+    public List<Producto> getProductos(){
+        List<ProductoEntity> productoEntities = productosRepository.findAll();
+        List<Producto> productos = new ArrayList<>();
+
+        for(ProductoEntity productoEntity : productoEntities){
+            Producto producto = new Producto();
+            producto.setId(productoEntity.getId());
+            producto.setNombre(productoEntity.getNombre());
+            producto.setPrecio(productoEntity.getPrecio());
+            producto.setStock(productoEntity.getStock());
+            productos.add(producto);
+        }
+        return productos;
+    }
+
+   
+
+}
